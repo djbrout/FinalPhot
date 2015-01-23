c28bea1b215f989e81b39b33d6146baa515ad964
Dillon Brout <djbrout@gmail.com>
2015-01-23 11:57:40 -0800

    zoom in on stamp

--- galsim_iter.py
+++ galsim_iter.py
@@ -39,8 +39,8 @@ class GalsimKernel:
                  , SN_DEC_guess = 0 # arsec from center of entire image (not stamp)
                  , SN_flux_guess = 0.0
                  , satisfactory = .8 # process is iterated until correlation reaches this value
-                 , stamp_RA = 500
-                 , stamp_DEC = 500
+                 , stamp_RA = 32
+                 , stamp_DEC = 32
                  , psf_file = ''
                  , outdir = None
                  ):
