dcc81e63c688983b679438875da61f63b12380f5
Dillon Brout <djbrout@gmail.com>
2015-01-26 08:07:37 -0800

    checking image 8

--- galsim_iter.py
+++ galsim_iter.py
@@ -318,7 +318,7 @@ if __name__=='__main__':
     #raw_input()
     #print exposures
 
-    image_num = 0
+    image_num = 8
 
     real_img_without_SN = str(image_paths[image_num]).strip('[').strip(']').replace("'",'')
     psf_file = real_img_without_SN.split('.')[0]+'.psf'
