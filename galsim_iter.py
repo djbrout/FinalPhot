9f187c4316aa99b3a41f747839508f0eafdea4dc
Dillon Brout <djbrout@gmail.com>
2015-02-24 18:36:01 -0800

    wtf

--- galsim_iter.py
+++ galsim_iter.py
@@ -733,7 +733,7 @@ if __name__=='__main__':
     #SN_counts_guesses = [0,1000,1000,1000]
 
     image_nums = [17,21,26]
-    SN_counts_guesses = [0,5000,5000]
+    SN_counts_guesses = [0,1,1]
 
     real_images, weights_files, psf_files, filters, galpos_ras, galpos_decs, exposure_nums, ccd_nums = read_query( query_file, image_dir, image_nums )
 
